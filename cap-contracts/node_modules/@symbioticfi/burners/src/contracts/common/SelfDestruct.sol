f42a2f37b385ae6b5f6fe9415397cf756ba6ea90
elminnyc99 <elminnyc99@gmail.com>
2025-07-16 08:18:46 +0700

    remove node modules commit

--- cap-contracts/node_modules/@symbioticfi/burners/src/contracts/common/SelfDestruct.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
-
-contract SelfDestruct {
-    constructor() payable {
-        selfdestruct(payable(address(this)));
-    }
-}
--- cap-contracts/node_modules/@symbioticfi/hooks/src/interfaces/networkRestakeDelegator/INetworkRestakeDecreaseHook.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import {IDelegatorHook} from "@symbioticfi/core/src/interfaces/delegator/IDelegatorHook.sol";
-
-interface INetworkRestakeDecreaseHook is IDelegatorHook {
-    error NotNetworkRestakeDelegator();
-}
